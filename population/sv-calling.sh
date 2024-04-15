dda0b3a2b80a03bfa72d655cef2028a77ee5d72b
lzjhehe <931023668@qq.com>
2024-04-15 10:09:29 +0800

    Update sv-calling.sh
    #234

--- population/sv-calling.sh
+++ population/sv-calling.sh
@@ -4,3 +4,4 @@ zcat /fq/${i}_1.clean.fastq.gz > /fq/${i}.fq
 zcat /fq/${i}_2.clean.fastq.gz >> /fq/${i}.fq
 PanGenie -f /ref/PNref -i /fq/${i}.fq -s ${i} -t 19 -o /res/${i} && rm /fq/${i}.fq
 bgzip -c -@ 5 /res/${i}_genotyping.vcf > /res/${i}_genotyping.vcf.gz && tabix -p /res/${i}_genotyping.vcf.gz
+bcftools merge -l list -O v -m all > all.SV.vcf
